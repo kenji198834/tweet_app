f74dbeeb38e60473e81ec5084e14b85564411bcb
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-02 18:26:33 +0900

    Usersコントローラーのeditアクションを呼び出すルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   post "users/create" => "users#create"
   get "signup" => "users#new"
-  
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
+  get "users/:id/edit" => "users#edit"
   
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
