bf509289eb80399389ab3fbe326ceffffecc367d
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-02 18:03:43 +0900

    ユーザーの保存に成功したときにサクセスメッセージが表示できるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+    end
   end
 
 end
