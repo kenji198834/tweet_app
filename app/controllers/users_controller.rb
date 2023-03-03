7c0be2b9da60b86e23b828b88e3a5f2cf6fc74a8
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-03 21:45:12 +0900

    ログイン成功時にサクセスメッセージが表示されるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      flash[:notice] = "ログインしました"
     end
   end
 
