667ed10eab1b8c6465b944b81419987caa0c7642
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-04 11:38:24 +0900

    ログインユーザーが見つからなかった場合の処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,8 @@ class UsersController < ApplicationController
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+        
       render("users/login_form")
     end
   end
