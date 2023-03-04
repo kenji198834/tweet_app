37416890a8183b99a8d19655ce1885efeab400b2
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-04 20:23:17 +0900

    投稿一覧ページにリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,6 +78,7 @@ class UsersController < ApplicationController
   def ensure_correct_user
     if @current_user.id != params[:id].to_i
       flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
     end
   end
 
