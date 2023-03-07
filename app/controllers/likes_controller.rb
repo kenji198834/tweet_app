0fbee28e5cdb933a597597877eb3833be5f06678
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-07 14:43:30 +0900

    post_idはparamsから取得して作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,8 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new（
-      user_id: @current_user.id
+      user_id: @current_user.id,
+      post_id: params[:post_id]
     ）
   end
 
