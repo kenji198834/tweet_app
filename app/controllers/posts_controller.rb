e310fafa93bc5f046a42ba94f82fbddba2ea119d
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-07 11:46:39 +0900

    投稿一覧ページにリダイレクトする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,6 +54,8 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     if @post.user_id != @current_user.id
       flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
 
 end
