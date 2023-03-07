cbeb2babba9622bcb13a4b3ec076b710b63c454a
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-03-07 15:02:00 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,7 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @like = Like.new（
-      user_id: @current_user.id,
-      post_id: params[:post_id]
-    ）
+    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
     redirect_to("/posts/#{params[:post_id]}")
   end
